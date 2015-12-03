f1808ac0d5097b0b565eb67f6de52506a30e8999
Dominik Kasprzak <dominik.kasprzak@progrupa.com>
2015-12-03 11:32:51 +0100

    Added LIN constants for ORDCHG message

--- src/EDI/Segments/LIN.php
+++ src/EDI/Segments/LIN.php
@@ -6,6 +6,11 @@ use EDI\Message\Segment;
 
 class LIN
 {
+    const ADDED = 1;
+    const DELETED = 2;
+    const CHANGED = 3;
+    const NO_ACTION = 4;
+
     public static function create($lineNo, $itemId)
     {
         $segment = new Segment('LIN');
